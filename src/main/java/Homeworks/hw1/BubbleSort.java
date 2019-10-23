47a87d5e2b0709772ea3685bd7767a62c1781561
Mikayil <mikayil.yusubov@gmail.com>
2019-10-23 17:46:26 +0400

    AA

---
+++ src/main/java/Homeworks/hw1/BubbleSort.java
@@ -0,0 +1,19 @@
+package Homeworks.hw1;
+
+public class BubbleSort {
+
+    public static void Sort(int[] intArray) {
+        int n = intArray.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (intArray[j - 1] > intArray[j]) {
+                    temp = intArray[j - 1];
+                    intArray[j - 1] = intArray[j];
+                    intArray[j] = temp;
+                }
+
+            }
+        }
+    }
+}
--- src/main/java/Homeworks/hw1/Homework.java
+++ src/main/java/Homeworks/hw1/Homework.java
@@ -27,25 +27,9 @@ public class Homework {
                 System.out.printf("Congratulations, %s", name);
                 intArray = Arrays.copyOf(intArray, intArray.length + 1);
                 intArray[intArray.length - 1] = player_number;
-                Sort(intArray);
+                BubbleSort.Sort(intArray);
                 System.out.println("\nYour numbers:" + Arrays.toString(intArray));
                 break;
-            }
-        }
-    }
-
-    private static void Sort(int[] intArray) {
-        int n = intArray.length;
-        int temp = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < (n - i); j++) {
-
-                if (intArray[j - 1] > intArray[j]) {
-                    temp = intArray[j - 1];
-                    intArray[j - 1] = intArray[j];
-                    intArray[j] = temp;
-                }
-
             }
         }
     }
